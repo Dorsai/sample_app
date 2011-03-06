4b25bf485973940f8cd0f1d47891a6a2df909d11
Skip Levens <skip@giantravens.com>
2011-03-06 12:32:56 -0600

    Done with static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 SampleApp::Application.routes.draw do
   get "pages/home"
-  get "pages/contact"
+  get "pages/about"
+  get "pages/contact" 
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
